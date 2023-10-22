e649b8fde4f71457aa2f040d7052bd95473a732a
Katerina Vishnyakova <katherine.vishnyakova@gmail.com>
2023-10-22 22:24:23 +0300

    Add task 1.7

--- Скрипт БД.sql
+++ Скрипт БД.sql
@@ -4,3 +4,12 @@ create table Product (
 	maker int,
 	model varchar(50)
 );
+
+-- 1.7 В репозитории SQL в файле “Скрипт БД” добавить в таблицу Product 
+-- колонку price (cтрока, 20 символов)
+
+alter table Product
+add price varchar (20);
+
+
+
