2f3071f0708aaea996b005e74df2d565395b8644
unknown <n_zarina@hotmail.com>
2023-10-22 17:35:49 +0300

    Add task 1.1

---
+++ Скрипт БД.sql
@@ -0,0 +1,6 @@
+--1.1 В репозитории SQL в файле “Скрипт БД” создать таблицу Product, 
+-- в которой есть колонки maker (число), model (строка 50 символов)
+create table Product (
+	maker int,
+	model varchar(50)
+);
