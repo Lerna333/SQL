db4cf4a41627802654677a166a88e046db1c3141
unknown <n_zarina@hotmail.com>
2023-12-06 13:01:11 +0300

    Add table Books

--- The_Literary_Loft.sql
+++ The_Literary_Loft.sql
@@ -1,1 +1,11 @@
 // The main document of 'The Literary Loft' web-service
+
+// Создать таблицу books для БД "The Literary Loft"
+create table books (
+	book_id int NOT NULL PRIMARY KEY,
+	title char(50),
+    author_id int,
+    genre_id int,
+    year_published int,
+    release_date int
+);
